d2b343da39c7cf03f11577d4caa7216ffc22f9ca
Andreas Koch <andyk7@gmail.com>
2012-04-19 15:15:49 +0200

    Finished the lesson about reading and writing XML

--- Lessons/Reading-and-Writing-XML/reading-xml.ps1
+++ Lessons/Reading-and-Writing-XML/reading-xml.ps1
@@ -1,1 +1,5 @@
-﻿
+﻿[xml] $xml = Get-Content sample.xml
+foreach ($setting in $xml.Settings.Entry)
+{
+	Write-Host "$($setting.psbase.InnerText) (Id: $($setting.Id))"
+}
--- Lessons/Reading-and-Writing-XML/writing-xml.ps1
+++ Lessons/Reading-and-Writing-XML/writing-xml.ps1
@@ -1,1 +1,31 @@
-﻿
+﻿# Read XML
+Write-Host @"
+Before:
+---------------------
+"@
+Get-Content sample.xml
+Write-Host
+
+[xml] $xml = Get-Content sample.xml
+
+# Add Entry
+$newEntry = $xml.CreateElement("Entry")
+$newEntry.psbase.InnerText = "Value 4"
+
+$idAttribute = $xml.CreateAttribute("id")
+$idAttribute.psbase.Value = "4"
+$newEntry.SetAttributeNode($idAttribute) | Out-Null
+
+$xml.Settings.AppendChild($newEntry) | Out-Null
+
+# Save XML
+$targetFile = Join-Path "$(Get-Location)" "sample-extended.xml"
+$xml.save($targetFile)
+
+# Display New XML
+Write-Host @"
+After:
+---------------------
+"@
+Get-Content $targetFile
+Write-Host
